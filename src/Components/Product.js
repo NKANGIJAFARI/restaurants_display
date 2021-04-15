f3fe1d3a4e6a0df9eccb2bae81766ff83efe6c75
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-15 05:25:13 +0400

    Added details display for each restaurant

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -9,7 +9,32 @@ const Product = ({
   deliveryCost,
   deliveryTime,
 }) => {
-  return <div className='product' id='product'></div>;
+  return (
+    <div className='product' id='product'>
+      <div className='product__imgWrapper'>
+        <img src={image} alt='' />
+      </div>
+      <div className='product__details'>
+        <div className='product__details--left'>
+          <h2>{title}</h2>
+          <p>{desc}</p>
+          <h6>{deliveryCost}</h6>
+          <p>{promotion}</p>
+        </div>
+        <div className='product__details--right'>
+          <span>
+            <i class='fas fa-star'></i>
+            <i class='fas fa-star'></i>
+            <i class='fas fa-star'></i>
+            <i class='fas fa-star'></i>
+            <i class='fas fa-star'></i>
+          </span>
+          <h6>{desc}</h6>
+          <h6>{deliveryTime}</h6>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
