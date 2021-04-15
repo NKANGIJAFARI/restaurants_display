84d2fd56a1721820e739bfb5e85924a5f73c892e
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-15 05:23:28 +0400

    Added a singular view of the restaurant

---
+++ src/Components/Product.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Product = ({
+  image,
+  desc,
+  title,
+  promotion,
+  rating,
+  deliveryCost,
+  deliveryTime,
+}) => {
+  return <div className='product' id='product'></div>;
+};
+
+export default Product;
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './components/Product';
 
 const HomeScreen = () => {
   return (
