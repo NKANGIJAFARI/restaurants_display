0198f58706b00b13203eff5ca7edc6f0d21a768a
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-15 05:39:01 +0400

    Created some dummy data to be used as restaurants data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
+import React from 'react';
+
+const App = () => {
   return <div className='App'>Reaxt app</div>;
-}
+};
 
 export default App;
---
+++ src/Data/data.js
@@ -0,0 +1,48 @@
+const products = [
+  {
+    image: '/images/testing.jpg',
+    name: 'Pizza',
+    title: 'Holly Grail Restaurant',
+    promotion: '20% off selected items',
+    desc: 'Arabic, lebanese, chinese',
+    rating: '4',
+    deliveryDetails: 'cash-on-delivery',
+    deliveryCost: 'Delivery: aed 10',
+    deliveryTime: 'Delivery with 25mins',
+  },
+  {
+    image: '/images/testing.jpg',
+    name: 'Pizza',
+    title: 'Holly Grail Restaurant',
+    promotion: '20% off selected items',
+    desc: 'Arabic, lebanese, chinese',
+    rating: '4',
+    deliveryDetails: 'cash-on-delivery',
+    deliveryCost: 'Delivery: aed 10',
+    deliveryTime: 'Delivery with 25mins',
+  },
+  {
+    image: '/images/testing.jpg',
+    name: 'Pizza',
+    title: 'Holly Grail Restaurant',
+    promotion: '20% off selected items',
+    desc: 'Arabic, lebanese, chinese',
+    rating: '4',
+    deliveryDetails: 'cash-on-delivery',
+    deliveryCost: 'Delivery: aed 10',
+    deliveryTime: 'Delivery with 25mins',
+  },
+  {
+    image: '/images/testing.jpg',
+    name: 'Pizza',
+    title: 'Holly Grail Restaurant',
+    promotion: '20% off selected items',
+    desc: 'Arabic, lebanese, chinese',
+    rating: '4',
+    deliveryDetails: 'cash-on-delivery',
+    deliveryCost: 'Delivery: aed 10',
+    deliveryTime: 'Delivery with 25mins',
+  },
+];
+
+export default products;
