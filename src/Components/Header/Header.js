14e7817318a705cd2f12d228077c7c4bb20291c6
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-15 07:28:40 +0400

    Added a header to the Homscreen which header will take the functionality of search

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import products from '../../Data/data';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header__logo'>
+        <span>SHISHEO</span>
+      </div>
+      <div className='header__search'>
+        <input
+          type='text'
+          placeholder='Search in chats'
+          onChange={(e) => {
+            searchProducts(e.target.value);
+            e.target.value ? setSearching(true) : setSearching(false);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
