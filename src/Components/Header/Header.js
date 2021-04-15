9c0d87eac9554df012045a3f621d776bd3fa710e
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-15 09:22:06 +0400

    Added styling to the header and links to navigate

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import products from '../../Data/data';
-
+import { Link } from 'react-router-dom';
+import './header.scss';
 const Header = () => {
   return (
     <div className='header'>
       <div className='header__logo'>
         <span>SHISHEO</span>
+      </div>
+      <div>
+        <nav>
+          <Link for='/'>HOME</Link>
+          <Link for='/map'>MAP</Link>
+        </nav>
       </div>
     </div>
   );
